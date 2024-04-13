abc3b84f153231f561c37f09bd64dce2220ac26d
AndreyMatuhin99 <tronder.yt@gmail.com>
2024-04-14 00:13:26 +0300

    Задание 5

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,48 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T* data;
+    int top;
+public:
+    TStack() {
+        data = new T[size];
+        top = -1;
+    }
+
+    ~TStack() {
+        delete[] data;
+    }
+
+    bool isEmpty() const {
+        return top == -1;
+    }
+
+    bool isFull() const {
+        return top == size - 1;
+    }
+
+    void push(const T& value) {
+        if (!isFull()) {
+            data[++top] = value;
+        }
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            return data[top--];
+        } else {
+            return T();
+        }
+    }
+
+    T get() const {
+        if (!isEmpty()) {
+            return data[top];
+        } else {
+            return T();
+        }
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
